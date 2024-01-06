a5fe2b2fd70507d3452eb27c808c5af213cafc21
tluwaniMS <tluwani2020@gmail.com>
2024-01-06 21:57:14 +0200

    adding-person-type

--- auxiliary_types/auxiliary_types.go
+++ auxiliary_types/auxiliary_types.go
@@ -3,3 +3,10 @@ package auxiliary_types
 type ResponseMessage struct {
 	Message string `json:"message"`
 }
+
+type Person struct {
+	Name    string `json:"name"`
+	Surname string `json:"surname"`
+	Gender  string `json:"gender"`
+	Age     int    `json:"age"`
+}
