2b094a131746294c2fd8941bc82a3d6d8809c1dd
tluwaniMS <tluwani2020@gmail.com>
2024-01-06 16:16:16 +0200

    first-phase-of-adding-router-set-up

--- server_set_up/server_set_up.go
+++ server_set_up/server_set_up.go
@@ -1,9 +1,19 @@
 package server_set_up
 
+import (
+	"basic-go-rest-api/first_controller"
+	"basic-go-rest-api/second_controller"
+	"github.com/gorilla/mux"
+)
+
 func StartServer() {
 
 }
 
 func ConfigureRoutes() {
+	router := mux.NewRouter()
+
+	firstController := router.PathPrefix("/api/first-controller").Subrouter()
+	secondController := router.PathPrefix("/api/second-controller").Subrouter()
 
 }
